16db464ad8703c0b4ef6c1d1045196de80f68b58
KristijanGolub <kristijan.golubic12@gmail.com>
2017-11-14 15:31:53 +0100

    Skoro konačno riješenje

--- program.cpp
+++ program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main ()
@@ -13,8 +14,21 @@ int main ()
                             {2,2,0,0,0,0,0,0,0,2},
                             {0,0,0,0,0,0,0,0,0,0},
                             {0,0,0,0,1,1,1,1,1,1}};
+cout << "    ";
 for (int i = 0; i < 10; i++)
 {
+    cout << i << " ";
+}
+cout << endl;
+cout << "    ";
+for (int i = 0; i < 10; i++)
+{
+    cout << "--";
+}
+cout << endl;
+for (int i = 0; i < 10; i++)
+{
+    cout << i << "|  ";
     for (int j = 0; j < 10; j++)
     {
         cout << polje2D[i][j]<< " ";
@@ -22,5 +36,32 @@ for (int i = 0; i < 10; i++)
     cout << endl;
 }
 
+int redak, stupac;
+int br_pogodjenih = 0;
+int na_redu = 1;
+int napada = 2;
+while(1)
+{
+    cout << "Igrac " << na_redu << "Napada igraca 2 " << napada;
+    cout << "Unesite redak: " << endl;
+    cin >> redak;
+    cout << "Unesite stupac: " << endl;
+    cin >> stupac;
+    if (polje2D[redak][stupac]==napada)
+    {
+      cout << "Pogodili ste neprijateljski brod: " << endl;
+      br_pogodjenih++;
+    }
+    else
+    {
+      cout << "Niste pogodili brod neprijatelja: " << endl;
+    }
+    if( br_pogodjenih == 5 )
+    {
+        cout << "igrac je pobjedio:" << endl;
+        break;
+    }
+    swap( na_redu, napada);
+}
 return 0;
 }
